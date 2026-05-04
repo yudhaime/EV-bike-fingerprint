6c18c583d75b06d67d5876529333dc427c420b31
yudhaime <36374524+yudhaime@users.noreply.github.com>
2026-05-04 10:09:29 +0700

    Update WiFi configuration and add reset pin

--- EV-fingerprint-web/config.h
+++ EV-fingerprint-web/config.h
@@ -1,14 +1,18 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// ================= WIFI =================
-#define WIFI_SSID "EV-Fingerprint"
-#define WIFI_PASS "YNTKTS"
+// ================= WIFI (DEFAULT AP MODE) =================
+#define DEFAULT_WIFI_SSID "EVFINGERPRINT"
+#define DEFAULT_WIFI_PASS "YNTKTS123"
+
+// ================= WIFI POWER SAVING =================
+#define WIFI_TIMEOUT_MS   300000  // 5 menit = 300000 ms
 
 // ================= PIN =================
 #define BUZZER_PIN 4
 #define RELAY_PIN 3
 #define TOUCH_PIN 7
+#define RESET_WIFI_PIN 5
 
 // ================= UART =================
 #define RX_PIN 20
@@ -24,6 +28,5 @@
 // ================= FINGERPRINT =================
 #define CONFIDENCE_THRESHOLD 40
 #define MAX_FINGERPRINT_ID   127
-#define MAX_USERS            4
 
 #endif
